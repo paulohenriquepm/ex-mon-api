4c71e7dc261743a85f7f1f29ab4c8c38ffc1c12d
Paulo Henrique <paulo.henriquepm@outlook.com>
2022-04-06 20:56:23 -0300

    test: add setup to put auth token on header

--- test/ex_mon_api_web/controllers/trainer_controller_test.exs
+++ test/ex_mon_api_web/controllers/trainer_controller_test.exs
@@ -1,14 +1,26 @@
 defmodule ExMonApiWeb.Controllers.TrainersControllerTest do
   use ExMonApiWeb.ConnCase
 
+  import ExMonApiWeb.Auth.Guardian
+
   alias ExMonApi.Schemas.Trainer
 
   describe "show/2" do
-    test "when exists a trainer with given id, it should return the trainer", %{conn: conn} do
+    setup %{conn: conn} do
       params = %{name: "Teste", password: "123456789"}
-      {:ok, %Trainer{id: id}} = ExMonApi.create_trainer(params)
+      {:ok, trainer} = ExMonApi.create_trainer(params)
+      {:ok, token, _claims} = encode_and_sign(trainer)
 
-      result = conn |> get(Routes.trainers_path(conn, :show, id)) |> json_response(:ok)
+      conn = put_req_header(conn, "authorization", "Bearer #{token}")
+
+      {:ok, conn: conn, trainer_id: trainer.id}
+    end
+
+    test "when exists a trainer with given id, it should return the trainer", %{
+      conn: conn,
+      trainer_id: trainer_id
+    } do
+      result = conn |> get(Routes.trainers_path(conn, :show, trainer_id)) |> json_response(:ok)
 
       assert %{"id" => id, "inserted_at" => _inserted_at, "name" => "Teste"} = result
     end
@@ -16,12 +28,14 @@ defmodule ExMonApiWeb.Controllers.TrainersControllerTest do
     test "when does not exist a trainer with given id, it should return the not found message", %{
       conn: conn
     } do
-      trainer_id = Ecto.UUID.generate()
+      wrong_trainer_id = Ecto.UUID.generate()
 
       result =
-        conn |> get(Routes.trainers_path(conn, :show, trainer_id)) |> json_response(:bad_request)
+        conn
+        |> get(Routes.trainers_path(conn, :show, wrong_trainer_id))
+        |> json_response(:bad_request)
 
-      assert %{"message" => "Trainer not found with id: #{trainer_id}"} == result
+      assert %{"message" => "Trainer not found with id: #{wrong_trainer_id}"} == result
     end
   end
 end
