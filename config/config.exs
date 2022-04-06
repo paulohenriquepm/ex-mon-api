33799f4ca76433f880b06a2acd90c4e4768ae735
Paulo Henrique <paulo.henriquepm@outlook.com>
2022-04-06 20:13:44 -0300

    feat: add guardian dep and config

--- config/config.exs
+++ config/config.exs
@@ -25,6 +25,10 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :ex_mon_api, ExMonApiWeb.Auth.Guardian,
+  issuer: "ex_mon_api",
+  secret_key: "4SLri6RnhFUXrWi0q9CLDKIqP3YJUqkJUrYwCuSakrsiaShNGl+p2bP0HCsGvMtj"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
